e34ebfc0fcc24a4429e8356cbd7a468ae2423f58
Charlie Bird <cbvvrd@gmail.com>
2025-01-24 15:16:44 +0000

    switching from main to initial build branch

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,14 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 function Header() {
     return (
         <>
-        <header className="flex px-5 py-3 border-b border-black">
+        <header className="flex justify-between pl-5 pr-8 py-3 border-b border-black">
             <img src="images/xact-logo-1.png" alt="Xact Developments logo" className="h-16"/>
-            <button>menu</button>
-            <div className="hidden ">
+            <button><FontAwesomeIcon icon={faBars} className="text-2xl text-slate-900"/> </button>
+            <div className="hidden">
                 <a href="#">Home</a>
                 <a href="#">Demo</a>
                 <a href="#">Contact</a>

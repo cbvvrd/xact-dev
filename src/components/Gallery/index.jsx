06989cffeef085b5560ac723353702a6a9e9f5b3
Charlie Bird <cbvvrd@gmail.com>
2025-01-28 18:08:53 +0000

    horizonal scrolling buttons added to gallery & scrollbar hidden

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 
 function Gallery() {
@@ -14,16 +15,39 @@ function Gallery() {
         });
     }, []);
 
+    const innerGallery = document.getElementById("innerGallery")
+    
+    const slideRight = () => {
+        let width = screen.width;
+        innerGallery.scrollLeft += width;
+    }
+
+    const slideLeft = () => {
+        let width = screen.width;
+        innerGallery.scrollLeft -= width;
+    }
+
+    // function timedScroll() {
+    //     slideRight;
+    // }
+     
+    // setTimeout(slideRight, 1000);
 
     return (
         <>
-            <div className="overflow-x-scroll whitespace-nowrap h-64 md:h-[32rem]">
+            <div className="flex items-center ">
+            <MdChevronLeft onClick={slideLeft} className=" absolute z-10 left-1 text-[1.5em] text-blue-400 bg-white border border-slate-300 rounded-full"/>
+                <div id="innerGallery" className="snap-x overflow-x-scroll scrollbar-hide whitespace-nowrap scroll-smooth h-64 md:h-[32rem]">
+                    
                 {gallery.map(item =>
-                    <div className="relative inline-block text-center md:w-screen ">
-                        <img src={item.image} className="object-cover h-64 md:h-[30rem] md:w-screen" />
-                        <p className="absolute top-1/4 md:left-1/4 py-2 text-wrap text-white bg-black bg-opacity-40 font-bold text-3xl">{item.text}</p>
+                    
+                    <div className="relative inline-block text-center">
+                        <img src={item.image} className="snap-center object-cover h-64 md:h-[30rem] md:w-screen" />
+                        <p className="absolute top-1/4 md:left-1/4 py-2 text-wrap text-white bg-black bg-opacity-40 mx-10 font-bold text-3xl">{item.text}</p>
                     </div>
                 )}
+            </div>
+            <MdChevronRight onClick={slideRight}  className="absolute right-1 text-[1.5em] z-10  text-blue-400 bg-white border border-slate-300 rounded-full"/>
             </div>
         </>
     );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,7 +11,7 @@ function Header() {
 
     return (
         <>
-            <header className="flex justify-between px-5 py-3 border-b border-black md:justify-around md:px-0">
+            <header className="flex justify-between px-5 py-1 border-b border-slate-600 md:justify-around md:px-0">
                 <img src="images/xact-logo-1.png" alt="Xact Developments logo" className="h-16 md:h-20"/>
                 <button onClick={() => {setMenuOpen(!menuOpen)}}><FontAwesomeIcon icon={faBars} className={menuOpen ? "text-2xl rotate-90 text-slate-900 md:hidden" : "text-2xl text-slate-900 md:hidden"}/> </button>
                 <nav className="hidden self-center text-lg md:flex gap-7">
@@ -21,7 +21,7 @@ function Header() {
                 </nav>    
             </header>
 
-            <div className={menuOpen ? "fixed top-0 right-0 flex flex-col items-center gap-1 pt-8 pl-10 pr-5  h-full bg-slate-800 text-white text-lg" : "hidden"}>
+            <div className={menuOpen ? "fixed z-20 top-0 right-0 flex flex-col items-center gap-1 pt-8 pl-10 pr-5  h-full bg-slate-800 text-white text-lg" : "hidden"}>
                 <button onClick={() => setMenuOpen(false)} className="self-end"><FontAwesomeIcon icon={faX} className="text-xl hover:text-blue-400 duration-75" /></button>
                 {/* <p className="underline">Navigate to:</p> */}
                 <nav className="flex flex-col items-end text-center gap-3 pt-5">

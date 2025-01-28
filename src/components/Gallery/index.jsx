2a62f438c42d49443df774fa8e114a42ec432e6a
Charlie Bird <cbvvrd@gmail.com>
2025-01-28 18:22:39 +0000

    responsive design added to gallery

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -33,22 +33,26 @@ function Gallery() {
      
     // setTimeout(slideRight, 1000);
 
+    const paused = false;
+
     return (
         <>
             <div className="flex items-center ">
-            <MdChevronLeft onClick={slideLeft} className=" absolute z-10 left-1 text-[1.5em] text-blue-400 bg-white border border-slate-300 rounded-full"/>
+            <MdChevronLeft onClick={slideLeft} className=" absolute z-10 left-1 md:left-3 text-[1.5em] md:text-[2.53em] text-blue-400 bg-white border border-slate-300 rounded-full"/>
                 <div id="innerGallery" className="snap-x overflow-x-scroll scrollbar-hide whitespace-nowrap scroll-smooth h-64 md:h-[32rem]">
                     
                 {gallery.map(item =>
                     
                     <div className="relative inline-block text-center">
-                        <img src={item.image} className="snap-center object-cover h-64 md:h-[30rem] md:w-screen" />
-                        <p className="absolute top-1/4 md:left-1/4 py-2 text-wrap text-white bg-black bg-opacity-40 mx-10 font-bold text-3xl">{item.text}</p>
+                        <img src={item.image} className="snap-center object-cover h-64 md:h-[31rem] md:w-screen" />
+                        <p className="absolute top-1/4 md:left-1/4 md:top-1/3 p-2 md:w-1/3 text-3xl text-wrap text-white bg-black bg-opacity-40 mx-10 font-bold md:text-5xl">{item.text}</p>
                     </div>
                 )}
             </div>
-            <MdChevronRight onClick={slideRight}  className="absolute right-1 text-[1.5em] z-10  text-blue-400 bg-white border border-slate-300 rounded-full"/>
+            <MdChevronRight onClick={slideRight}  className="absolute right-1 md:right-3 text-[1.5em] md:text-[2.53em] z-10  text-blue-400 bg-white border border-slate-300 rounded-full"/>
+            
             </div>
+            {paused ? <p>hello</p> : <p>goodbye</p>}
         </>
     );
 }

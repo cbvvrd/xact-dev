b7a9782dd516e36a5263acea04ce504a64c7e7b8
Charlie Bird <cbvvrd@gmail.com>
2025-01-31 19:17:08 +0000

    updating homepage links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,10 +12,10 @@ function Header() {
     return (
         <>
             <header className="flex justify-between px-5 py-1 border-b border-slate-600 md:justify-around md:px-0">
-                <Link to="/xact-dev"><img src="images/xact-logo-1.png" alt="Xact Developments logo" className="h-16 md:h-20"/></Link>
+                <Link to="/xact-dev/"><img src="images/xact-logo-1.png" alt="Xact Developments logo" className="h-16 md:h-20"/></Link>
                 <button onClick={() => {setMenuOpen(!menuOpen)}}><FontAwesomeIcon icon={faBars} className={menuOpen ? "text-2xl rotate-90 text-slate-900 md:hidden" : "text-2xl text-slate-900 md:hidden"}/> </button>
                 <nav className="hidden self-center text-lg md:flex gap-7">
-                    <Link to="/xact-dev" className="hover:text-blue-400 duration-75">Home</Link>
+                    <Link to="/xact-dev/" className="hover:text-blue-400 duration-75">Home</Link>
                     <Link to="/xact-dev/demo" className="hover:text-blue-400 duration-75">Demo</Link>
                     <Link to="/xact-dev/contact" className="hover:text-blue-400 duration-75">Contact</Link>
                 </nav>    
@@ -25,7 +25,7 @@ function Header() {
                 <button onClick={() => setMenuOpen(false)} className="self-end"><FontAwesomeIcon icon={faX} className="text-xl hover:text-blue-400 duration-75" /></button>
                 {/* <p className="underline">Navigate to:</p> */}
                 <nav className="flex flex-col items-end text-center gap-3 pt-5">
-                    <Link to="/xact-dev" className="hover:text-blue-400 duration-75">Home</Link>
+                    <Link to="/xact-dev/" className="hover:text-blue-400 duration-75">Home</Link>
                     <Link to="/xact-dev/demo" className="hover:text-blue-400 duration-75">Demo</Link>
                     <Link to="/xact-dev/contact" className="hover:text-blue-400 duration-75">Contact</Link>
                 </nav>

d547f47930eac705e641f3964774879c92f0126d
Charlie Bird <cbvvrd@gmail.com>
2025-02-07 15:37:42 +0000

    Updated ContactUs component so the whole thing is a link to contact page. Also has underline on hover

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 function ContactUs() {
     return (
-        <div className="flex flex-col items-center text-center text-white gap-2 py-8 px-5 bg-gradient-to-r from-purple-500 to-red-500">
-            <h4 className="text-lg font-semibold">Contact Us!</h4>
-            <p>If you would like to get a quote or discuss what we can offer your business, please <Link to="/xact-dev/contact" className="underline"> get in touch</Link></p>
+        <Link to="/xact-dev/contact">
+        <div className="flex flex-col items-center text-center text-white gap-2 py-8 px-5 bg-gradient-to-r from-purple-500 to-red-500 hover:underline">
+            <h4 className="text-xl font-semibold">Contact Us!</h4>
+            <p>If you would like to get a quote or discuss what we can offer your business, please get in touch.</p>
         </div>
+        </Link>
     );   
 }
 

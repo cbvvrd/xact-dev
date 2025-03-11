fafcd1176f2af9e42508bb70ee6b55f062b74cef
CBird <cbvvrd@gmail.com>
2025-03-11 19:45:29 +0000

    updated styling on email address

--- src/components/ContactDetails/index.jsx
+++ src/components/ContactDetails/index.jsx
@@ -4,9 +4,10 @@ import ContactForm from "../ContactForm";
 function ContactDetails() {
     return (
         <div className="flex flex-col justify-stretch gap-5 mt-8 mb-10 mx-3 md:mx-24">
-            <p className="text-lg text-center">Email: 
+            <div className="text-lg text-center flex justify-center gap-2">
+                <p>Email:</p>  
                 <a href="mailto:contact@xactdev.co.uk" className="underline underline-offset-2 hover:underline-offset-4 hover:text-purple-900 duration-150">contact@xactdev.co.uk</a>
-            </p>
+            </div>
             <ContactForm />
             <div className="flex justify-center gap-5 text-xl">
                 <p className="hover:text-purple-900">Github <FaGithub /></p>
